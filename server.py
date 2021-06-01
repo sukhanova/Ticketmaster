b8882437a9c00f4deacade9da14fb9c77792834b
Vira Sukhanova <verusia.s@gmail.com>
2021-05-31 22:16:53 -0500

    Completed web app

---
+++ server.py
@@ -0,0 +1,82 @@
+from flask import Flask, render_template, request
+
+from pprint import pformat
+import os
+import requests
+
+
+app = Flask(__name__)
+app.secret_key = 'SECRETSECRETSECRET'
+
+# This configuration option makes the Flask interactive debugger
+# more useful (you should remove this line in production though)
+app.config['PRESERVE_CONTEXT_ON_EXCEPTION'] = True
+
+
+API_KEY = os.environ['TICKETMASTER_KEY']
+
+
+@app.route('/')
+def homepage():
+    """Show homepage."""
+
+    return render_template('homepage.html')
+
+
+@app.route('/afterparty')
+def show_afterparty_form():
+    """Show event search form"""
+
+    return render_template('search-form.html')
+
+
+@app.route('/afterparty/search')
+def find_afterparties():
+    """Search for afterparties on Eventbrite"""
+
+    keyword = request.args.get('keyword', '')
+    postalcode = request.args.get('zipcode', '')
+    radius = request.args.get('radius', '')
+    unit = request.args.get('unit', '')
+    sort = request.args.get('sort', '')
+
+    url = 'https://app.ticketmaster.com/discovery/v2/events'
+    payload = {'apikey': API_KEY,
+               'keyword': keyword,
+               'postalcode': postalcode,
+               'radius': radius,
+               'unit': unit,
+               'sort': sort}
+
+    
+    response = requests.get(url, params=payload);
+    data = response.json();
+    events = data['_embedded']['events'];
+
+    return render_template('search-results.html',
+                           pformat=pformat,
+                           data=data,
+                           results=events)
+
+
+@app.route('/event/<id>')
+def get_event_details(id):
+    """View the details of an event."""
+
+    # TODO: Finish implementing this view function
+    url = f'https://app.ticketmaster.com/discovery/v2/events/{id}'
+    payload = {'apikey': API_KEY}
+   
+    res = requests.get(url, params=payload)
+    
+    event = res.json()
+    description = event['description']
+   
+    return render_template('event-details.html',
+                           event=event,
+                           description=description)
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0')
